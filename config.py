7136602ce8a9e8244b2327eb0dfcc3892b22403f
gabe-lepo <gabelepoutre@gmail.com>
2023-07-21 00:46:21 -0400

    removed config dict, just use constants

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-config = {}
-
-WIDTH = config.get('WIDTH', 1000)
-HEIGHT = config.get('HEIGHT', 1000)
-REFRESH_MS = config.get('REFRESH_MS', 60)
-SIZE = config.get('SIZE', 15)
-AGG_WEIGHT = config.get('AGG_WEIGHT', 0.1)
-DEF_WEIGHT = config.get('DEF_WEIGHT', 0.05)
-STD_WEIGHT = config.get('STD_WEIGHT', 0.2)
+WIDTH = 1000
+HEIGHT = 1000
+REFRESH_MS = 60
+SIZE = 50
+AGG_WEIGHT = 0.1
+DEF_WEIGHT = 0.05
+STD_WEIGHT = 0.2
